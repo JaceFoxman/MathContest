26c95f2c13db091c18314e3d7aefeae901153ef0
Jason Permann <JaceFoxman@gmail.com>
2025-04-06 14:07:41 -0600

    Rename Form, add header, and turn on option compare, strict, and explicit

--- MathContest/MathContestForm.vb
+++ MathContest/MathContestForm.vb
@@ -2,7 +2,11 @@
 'Spring 2025
 'RCET2265
 'Math Contest
-'
+'https://github.com/JaceFoxman/MathContest.git
+
+Option Compare Text
+Option Strict On
+Option Explicit On
 
 Public Class MathContestForm
     Private Sub MathContestForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
