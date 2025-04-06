09e7ece8a9e4dcfab1cd76e999817cc2a5716a23
Jason Permann <JaceFoxman@gmail.com>
2025-04-06 14:06:04 -0600

    Add project files.

---
+++ MathContest/MathContestForm.vb
@@ -0,0 +1,11 @@
+﻿'Jason Permann
+'Spring 2025
+'RCET2265
+'Math Contest
+'
+
+Public Class MathContestForm
+    Private Sub MathContestForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
