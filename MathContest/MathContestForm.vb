bffac761e2f9627ead01f6fc5bb2d7c8ebe21a60
Jason Permann <JaceFoxman@gmail.com>
2025-04-06 19:42:22 -0600

    Add Comments, Add Student Answer string protection

--- MathContest/MathContestForm.vb
+++ MathContest/MathContestForm.vb
@@ -9,6 +9,9 @@ Option Strict On
 Option Explicit On
 
 Public Class MathContestForm
+    ''' <summary>
+    ''' On first load set these pararameters.
+    ''' </summary>
     Sub SetDefaults()
         NameTextBox.Text = ""
         AgeTextBox.Text = ""
@@ -26,58 +29,98 @@ Public Class MathContestForm
         SummeryButton.Enabled = False
         SubmitButton.Enabled = False
     End Sub
-
+    ''' <summary>
+    ''' keeps track of correct answers and total attempts then displays
+    ''' </summary>
     Sub Summary()
         Dim _Summary As String
         _Summary = $"{NameTextBox.Text} got {ScoreCounter(, True)} out of {AttemptCounter(, True)}"
         MsgBox(_Summary, MsgBoxStyle.Information, "Total Score")
     End Sub
-
+    ''' <summary>
+    ''' Checks to see if student answer is correct or incoorect then displays meesage box depending on outcome
+    ''' </summary>
     Sub StudentAnswer()
         Dim message As String
+        Dim errormessage As String
         If StudentAnswerTextBox.Enabled = True Then
-            If CInt(StudentAnswerTextBox.Text) = MathProblem() Then
-                message = "Your answer was correct!"
-                MsgBox(message, MsgBoxStyle.Exclamation, "Congradulations!")
-            ElseIf CInt(StudentAnswerTextBox.Text) <> MathProblem() Then
+            If IsNumeric(StudentAnswerTextBox.Text) = False Then
+                errormessage = "Must provide numeric value"
+                MsgBox(errormessage, MsgBoxStyle.Critical, "Error")
+            ElseIf IsNumeric(AgeTextBox.Text) = True Then
+                If CInt(StudentAnswerTextBox.Text) = MathProblem() Then
+                    message = "Your answer was correct!"
+                    MsgBox(message, MsgBoxStyle.Exclamation, "Congradulations!")
+                ElseIf CInt(StudentAnswerTextBox.Text) <> MathProblem() Then
 
-                message = $"Sorry the correct answer is {MathProblem()}"
-                MsgBox(message, MsgBoxStyle.Question, "Better Luck Next Time.")
+                    message = $"Sorry the correct answer is {MathProblem()}"
+                    MsgBox(message, MsgBoxStyle.Question, "Better Luck Next Time.")
+                End If
             End If
+            StudentAnswerTextBox.Focus()
         End If
     End Sub
-
+    ''' <summary>
+    ''' Keeps track of current score witout running submit code
+    ''' </summary>
+    ''' <param name="clear"></param>
+    ''' <param name="CurrentScore"></param>
+    ''' <returns></returns>
     Function ScoreCounter(Optional clear As Boolean = False, Optional CurrentScore As Boolean = False) As Integer
         Static score As Integer
+        Dim errormessage As String
         If StudentAnswerTextBox.Enabled = True Then
-            If clear = False And CurrentScore = False Then
-                If CInt(StudentAnswerTextBox.Text) = MathProblem() Then
-                    score += 1
-                ElseIf CInt(StudentAnswerTextBox.Text) <> MathProblem() Then
-                    score += 0
+            If IsNumeric(StudentAnswerTextBox.Text) = False Then
+                StudentAnswerTextBox.Focus()
+                errormessage = "Must provide numeric value"
+                MsgBox(errormessage, MsgBoxStyle.Critical, "Error")
+            ElseIf IsNumeric(AgeTextBox.Text) = True Then
+                If clear = False And CurrentScore = False Then
+                    If CInt(StudentAnswerTextBox.Text) = MathProblem() Then
+                        score += 1
+                    ElseIf CInt(StudentAnswerTextBox.Text) <> MathProblem() Then
+                        score += 0
+                    End If
+                ElseIf clear = True Then
+                    score = 0
                 End If
-            ElseIf clear = True Then
-                score = 0
+                Return score
             End If
-            Return score
         End If
     End Function
+    ''' <summary>
+    ''' Keeps track of current attempts without running submit code
+    ''' </summary>
+    ''' <param name="clear"></param>
+    ''' <param name="CurrentAttempt"></param>
+    ''' <returns></returns>
     Function AttemptCounter(Optional clear As Boolean = False, Optional CurrentAttempt As Boolean = False) As Integer
         Static attempts As Integer
+        Dim errormessage As String
+
         If StudentAnswerTextBox.Enabled = True Then
-            If clear = False And CurrentAttempt = False Then
-                If CInt(StudentAnswerTextBox.Text) = MathProblem() Then
-                    attempts += 1
-                ElseIf CInt(StudentAnswerTextBox.Text) <> MathProblem() Then
-                    attempts += 1
+            If IsNumeric(StudentAnswerTextBox.Text) = False Then
+                StudentAnswerTextBox.Focus()
+                errormessage = "Must provide numeric value"
+                MsgBox(errormessage, MsgBoxStyle.Critical, "Error")
+            ElseIf IsNumeric(AgeTextBox.Text) = True Then
+                If clear = False And CurrentAttempt = False Then
+                    If CInt(StudentAnswerTextBox.Text) = MathProblem() Then
+                        attempts += 1
+                    ElseIf CInt(StudentAnswerTextBox.Text) <> MathProblem() Then
+                        attempts += 1
+                    End If
+                ElseIf clear = True Then
+                    attempts = 0
                 End If
-            ElseIf clear = True Then
-                attempts = 0
+                Return attempts
             End If
-            Return attempts
         End If
     End Function
-
+    ''' <summary>
+    ''' Checks user input to make sure values are correct
+    ''' </summary>
+    ''' <returns></returns>
     Function UserInput() As Boolean
         Dim checkPassed As Boolean = True
         Dim errormessage As String
@@ -113,7 +156,10 @@ Public Class MathContestForm
         End If
         Return checkPassed
     End Function
-
+    ''' <summary>
+    ''' Runs math function with both RNG numbers
+    ''' </summary>
+    ''' <returns></returns>
     Function MathProblem() As Integer
         Dim answer As Integer
         If AddRadioButton.Checked = True Then
@@ -127,6 +173,7 @@ Public Class MathContestForm
         End If
         Return answer
     End Function
+    'Math Operators__________________________________________________________________________
     Function Add() As Integer
         Dim sum As Integer
         Dim number1 As Integer
@@ -138,7 +185,6 @@ Public Class MathContestForm
 
         Return sum
     End Function
-
     Function Subtract() As Integer
         Dim difference As Integer
         Dim number1 As Integer
@@ -161,7 +207,6 @@ Public Class MathContestForm
 
         Return product
     End Function
-
     Function Divide() As Integer
         Dim quotient As Integer
         Dim number1 As Integer
@@ -174,7 +219,12 @@ Public Class MathContestForm
 
         Return quotient
     End Function
-
+    ''' <summary>
+    ''' Generates random number
+    ''' </summary>
+    ''' <param name="min"></param>
+    ''' <param name="max"></param>
+    ''' <returns></returns>
     Function RNG(min As Integer, max As Integer) As Integer
         Dim value As Single
         Randomize()
@@ -184,6 +234,7 @@ Public Class MathContestForm
         Return CInt(Math.Ceiling(value))
     End Function
 
+    'Event Handelers___________________________________________________________________________
     Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
         If UserInput() = True Then
             StudentAnswer()
@@ -199,7 +250,6 @@ Public Class MathContestForm
             DivideRadioButton.Enabled = True
         End If
     End Sub
-
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         NameTextBox.Text = ""
         AgeTextBox.Text = ""
@@ -222,7 +272,7 @@ Public Class MathContestForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
-
+    'Radio Button Swapping_____________________________________________________________________
     Private Sub AddRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles AddRadioButton.CheckedChanged
         If StudentAnswerTextBox.Enabled = True Then
             FirstNumberTextBox.Text = $"{(RNG(1, 20))}"
@@ -230,22 +280,23 @@ Public Class MathContestForm
         ElseIf StudentAnswerTextBox.Enabled = False Then
         End If
     End Sub
-
     Private Sub SubtractRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles SubtractRadioButton.CheckedChanged
         FirstNumberTextBox.Text = $"{(RNG(1, 20))}"
         SecondNumberTextBox.Text = $"{(RNG(1, 20))}"
     End Sub
-
     Private Sub MultiplyRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles MultiplyRadioButton.CheckedChanged
         FirstNumberTextBox.Text = $"{(RNG(1, 20))}"
         SecondNumberTextBox.Text = $"{(RNG(1, 20))}"
     End Sub
-
     Private Sub DivideRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles DivideRadioButton.CheckedChanged
         FirstNumberTextBox.Text = $"{(RNG(1, 20))}"
         SecondNumberTextBox.Text = $"{(RNG(1, 20))}"
     End Sub
-
+    ''' <summary>
+    ''' initializes submit button when text is added
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub NameTextBox_TextChanged(sender As Object, e As EventArgs) Handles NameTextBox.TextChanged
         SubmitButton.Enabled = True
     End Sub
